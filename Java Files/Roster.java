af5df17398a372e50811e9a4990adba712a9bb45
brentli17 <44733139+brentli17@users.noreply.github.com>
2020-11-18 03:54:40 -0700

    added openFileChooser

--- Java Files/Roster.java
+++ Java Files/Roster.java
@@ -1,5 +1,18 @@
 //Load a Roster function under File tab
 
-public class Roster{
-    //TO BE IMPLEMENTED
+import java.io.*; 
+import javax.swing.*; 
+import java.awt.event.*; 
+import javax.swing.filechooser.*; 
+
+class Roster {
+    //the main function is used for testing purposes only
+    public static void main(String args[]){
+        openFileChooser();
+    }
+
+    public static void openFileChooser(){
+        JFileChooser jFile = new JFileChooser();
+        jFile.showOpenDialog(null); //show open dialog
+    }
 }
