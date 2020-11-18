70a2aada18cc8e53616f6b1e75632260b6d6f0d4
Preet Patel <44732297+Preethum@users.noreply.github.com>
2020-11-18 03:43:53 -0800

    Added Full Menug Layout

--- Java Files/GUI.java
+++ Java Files/GUI.java
@@ -1,14 +1,34 @@
 // Used for the Graphical User Interface
 import javax.swing.*;
-public class gui{
+public class GUI{
     public void Model(){
-        JFrame f=new JFrame("CSE360 Final Project");
-        JButton b=new JButton("click");
-        b.setBounds(130,100,100, 40);
-        f.add(b);
-        f.setSize(400,500);
-        f.setLayout(null);
-        f.setVisible(true);
+        JMenu menu1, menu2;  
+        JMenuItem i1, i2, i3, i4, i5;  
+         
+        JFrame f= new JFrame("CSE360 Final Project");  
+        JMenuBar mb=new JMenuBar();  
+        menu1=new JMenu("File");   
+        i1=new JMenuItem("Load a Roster");  
+        i2=new JMenuItem("Add Attendance");  
+        i3=new JMenuItem("Save");  
+        i4=new JMenuItem("Plot Data"); 
+        
+        menu2 = new JMenu("About");
+     
+        menu1.add(i1); 
+        menu1.add(i2); 
+        menu1.add(i3);  
+        menu1.add(i4); 
+        mb.add(menu1);
+        mb.add(menu2);  
+
+        f.setJMenuBar(mb);  
+        f.setSize(400,400);  
+        f.setLayout(null);  
+        f.setVisible(true);  
+
+        
+        
     }
     
 }
