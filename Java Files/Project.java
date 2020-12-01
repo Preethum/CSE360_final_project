ad5471dc25e518bb13281435537913b425ecd5b8
brentli17 <44733139+brentli17@users.noreply.github.com>
2020-11-30 18:54:15 -0700

    added project member names per specifications

--- Java Files/Project.java
+++ Java Files/Project.java
@@ -1,4 +1,13 @@
 //Main file to handle user interaction
+/**
+ * Team Members:
+ * Brent Li             btli1   
+ * Preet Patel          ppatel39
+ * Martin Ople          mople
+ * Chase Kimball        ckimbal3
+ * Andrew Lauricella    allauric
+ * @param args
+ */
   
 class Project{
     public static void main (String[] args){
